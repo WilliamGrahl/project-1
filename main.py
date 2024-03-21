c61bd49cd63aecbbec4610d3c3ebb5b2bfa1f180
WilliamGrahl <159275819+WilliamGrahl@users.noreply.github.com>
2024-03-21 17:49:07 +0000

    In this update I have made a new function to the project and that is to move stuff around in the To do list. I have also made some bug fixes and made the list more readable

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
-print("My do list \n")
+print("My to do list \n")
 The_List = []
+didChange = False
+
 while True:
-    print("Do you want to add or delete something to your list? Add/remove")
-    didChange = False
-    number = 0
+    print("Do you want to add, delete or move something in your list?")
+    for index, todo in enumerate(The_List):
+        print(index+1,". ", todo)
     myAnswer = input()
+
     if myAnswer.lower() == "add":
-        print("What do you want to add?")
+        print("What do you want to add? \n")
         myAnswer2 = input()
         The_List.append(myAnswer2)
-        number += 1
         didChange = True
-        for (index, todo) in enumerate(The_List):
-            print(index+1, todo)
-    elif myAnswer.lower() == "remove":
-        number -= 1
-        print("Which item do you want to remove? type in the number of the item")
+        for index, todo in enumerate(The_List):
+            print(index+1,". ", todo)
+
+    elif myAnswer.lower() == "delete":
+        print("Which item do you want to delete? type in the number of the item \n")
         myAnswer3 = int(input())
         del The_List[myAnswer3-1]
         didChange = True
-        for (index, todo) in enumerate(The_List):
-            print(index+1, todo)
+        for index, todo in enumerate(The_List):
+            print(index+1,". ", todo)
+
+    elif myAnswer.lower() == "move":
+        print("Which item do you want to move? type in the number of the item \n")
+        myAnswer4 = int(input())
+        print("Now type in the number you want to move it to \n")
+        myAnswer5 = int(input())
+        index_to_move = myAnswer4 - 1
+        element = The_List.pop(index_to_move)
+        new_position = myAnswer5 - 1
+        The_List.insert(new_position, element)
+        didChange = True
+        for index, todo in enumerate(The_List):
+            print(index+1,". ", todo)
+
     else:
-        print("Im sorry, I don't understand")
+        print("Im sorry, I don't understand \n")
 
     if didChange == True:
-        print("Do you want to exit? y/n")
-        myAnswer4 = input()
-        if myAnswer4.lower() == "y":
-            print("Okey, Bye!")
-            for (index, todo) in enumerate(The_List):
-                print(index+1, todo)
-            exit()
+        print("Do you want to exit? y/n \n")
+        myAnswer6 = input()
+        if myAnswer6.lower() == "y":
+            print("Okey, Bye! \n")
+            for index, todo in enumerate(The_List):
+                print(index+1,". ", todo)
+            break
         else:
-            continue
+            didChange = False
